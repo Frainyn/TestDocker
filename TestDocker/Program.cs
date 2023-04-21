4119d774418ce1c8945d67c53b89097768747be4
Max <frainyn.work@gmail.com>
2023-04-21 15:31:29 +0900

    update

--- TestDocker/Program.cs
+++ TestDocker/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,7 +12,23 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+string path = "Static";
+DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+if (!dirInfo.Exists)
+{
+    dirInfo.Create();
+}
+
 app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Static")),
+    RequestPath = "/StaticFiles"
+});
 
 app.UseRouting();
 
